eae1c7cd7fbb48d546fe600bcb00b9ac2170bb52
Angular CLI <angular-cli@angular.io>
2017-04-20 11:02:50 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SpaceMayhemPage } from './app.po';
+
+describe('space-mayhem App', () => {
+  let page: SpaceMayhemPage;
+
+  beforeEach(() => {
+    page = new SpaceMayhemPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
